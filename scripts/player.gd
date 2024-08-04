a7e85fb8f328bb9ce516c742cbec2239cdb5cb6b
Clara.Morrissey <clara-doreen.morrissey@stud.uni-bamberg.de>
2024-08-04 21:52:27 +0200

    makes fist-interaction work

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,26 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var is_left: bool = true
+@export var sleeve_texture: Texture
+
 
 const SPEED = 300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var character_input: CharacterInput = $CharacterInput
+@onready var fist = $fist
+@onready var sleeve = $fist/sleeve
+
+
+func _ready():
+	if is_left: 
+		fist.scale.x = 1
+	else: 
+		fist.scale.x = -1
+	
+	sleeve.texture = sleeve_texture
 
 func _physics_process(delta):
 	# Add the gravity.
